5d2b320adc47862f1bfe789777a774b74dfa9434
david <david.alonso.su@gmail.com>
2022-02-08 10:18:30 +0100

    Change project to Blazor Server

---
+++ Data/PooLandDbContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace PooLandApp.Data
+{
+    public partial class PooLandDbContext : DbContext
+    {
+        public PooLandDbContext()
+        {
+        }
+
+        public PooLandDbContext(DbContextOptions<PooLandDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Poodatum> Poodata { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=poolanddb;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Poodatum>(entity =>
+            {
+                entity.ToTable("poodata");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Date).HasColumnName("date");
+
+                entity.Property(e => e.Description)
+                    .HasMaxLength(50)
+                    .HasColumnName("description");
+
+                entity.Property(e => e.Latitude).HasColumnName("latitude");
+
+                entity.Property(e => e.Longitude).HasColumnName("longitude");
+
+                entity.Property(e => e.Photo).HasColumnName("photo");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Data/Poodatum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PooLandApp.Data
+{
+    public partial class Poodatum
+    {
+        public int Id { get; set; }
+        public DateTime? Date { get; set; }
+        public string Description { get; set; } = null!;
+        public string Photo { get; set; } = null!;
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+    }
+}
